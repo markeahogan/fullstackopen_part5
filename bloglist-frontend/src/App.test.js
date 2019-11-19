ba4a7b743d3a182edd078804071e0891dad75a63
Mark Hogan <mark@popupasylum.co.uk>
2019-11-19 22:28:09 +0000

    5.16, 5.17 App component tests

--- bloglist-frontend/src/App.test.js
+++ bloglist-frontend/src/App.test.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import {render, waitForElement} from '@testing-library/react';
-jest.mock('.services/blogs');
-import App from '.App';
+jest.mock('./services/blogs');
+import App from './App';
+import blogService from './services/blogs';
 
 describe('App', () => {
-    test('only show login form when no user', () => {
+    test('only show login form when no user', async () => {
         const app = render(<App />);
         app.rerender(<App />);
-
-        expect(app.container).toHaveTextContent('login');
-        expect(app,container).not.toHaveTextContent('blogs');
+        const login = await app.findByText('login');
+        expect(login).toBeDefined();
     });
 
-    test('show blogs when logged in', () => {        
+    test('show blogs when logged in', async () => {      
+        
+        const USER_LOCAL = 'loggedBlogUser';
+        window.localStorage.setItem(USER_LOCAL, JSON.stringify({name:"TestUser"}));
+        
         const app = render(<App />);
         app.rerender(<App />);
+        
+        const login = await app.findByText('TestUser', {exact:false});
 
-        expect(app.container).toHaveTextContent('login');
-        expect(app,container).not.toHaveTextContent('blogs');
+        const blogs = await blogService.getAll();
+
+        const blog = app.getByText(blogs[0].title, {exact:false})
+
+        expect(login).toBeDefined();
+        expect(blog).toBeDefined();
     });
 });
